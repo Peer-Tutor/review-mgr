b342e43c463da4b1dceacefc8d1b7533b817cefa
Mona Lisa <xhncs123456@gmail.com>
2022-09-27 12:21:23 +0800

    no message

--- src/main/java/com/peertutor/ReviewMgr/controller/ReviewController.java
+++ src/main/java/com/peertutor/ReviewMgr/controller/ReviewController.java
@@ -14,6 +14,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 import java.util.Map;
 
 @Controller
--- src/main/java/com/peertutor/ReviewMgr/mapper/ReviewMapper.java
+++ src/main/java/com/peertutor/ReviewMgr/mapper/ReviewMapper.java
@@ -2,9 +2,21 @@ package com.peertutor.ReviewMgr.mapper;
 
 import com.peertutor.ReviewMgr.dto.ReviewDTO;
 import com.peertutor.ReviewMgr.model.Review;
+import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface ReviewMapper extends EntityMapper<ReviewDTO,Review>{
+	Review toEntity(ReviewDTO reviewDTO);
 
+	ReviewDTO toDto(Review review);
+
+    default Review fromId(Long id) {
+        if (id == null) {
+            return null;
+        }
+        Review review = new Review();
+        review.setId(id);
+        return review;
+    }
 }
 
