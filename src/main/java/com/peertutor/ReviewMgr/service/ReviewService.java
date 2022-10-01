6692c1d8fe71ae4badd9745cf9e77c4c0820b343
Mona Lisa <xhncs123456@gmail.com>
2022-10-01 23:06:04 +0800

    no message

--- src/main/java/com/peertutor/ReviewMgr/service/ReviewService.java
+++ src/main/java/com/peertutor/ReviewMgr/service/ReviewService.java
@@ -4,21 +4,26 @@ import com.peertutor.ReviewMgr.dto.ReviewDTO;
 import com.peertutor.ReviewMgr.mapper.ReviewMapper;
 import com.peertutor.ReviewMgr.model.Review;
 import com.peertutor.ReviewMgr.model.viewmodel.request.ReviewReq;
+import com.peertutor.ReviewMgr.model.viewmodel.request.TuitionOrderReq;
+import com.peertutor.ReviewMgr.service.TuitionOrderSvc;
 import com.peertutor.ReviewMgr.repository.ReviewRepository;
 
 public abstract class ReviewService implements ReviewRepository {
 	private static final Logger logger = LoggerFactory.getLogger(ReviewService .class);
     private final ReviewMapper reviewMapper;
     private ReviewRepository reviewRepository;
+    private TuitionOrderSvc tuitionOrderSvc;
     
     public ReviewDTO addReview(ReviewReq req) {
     	Review reviewMgr = reviewRepository.findById(req.id);
     	if(reviewMgr == null) {
     		reviewMgr = new Review();
     	}
-    	
-    	reviewMgr.setTutorID(req.tutorID);
-    	reviewMgr.settutionOrderID(req.tutionOrderID);
+    	Long tutionOrdID = tuitionOrderSvc.getTuitionOrderId();
+    	Long tutirID = tuitionOrderSvc.getTutorId();
+
+    	reviewMgr.setTutorID(tutirID);
+    	reviewMgr.settutionOrderID(tutionOrdID);
     	reviewMgr.setRating(req.rating);
     	reviewMgr.setComment(req.comment);
     	
